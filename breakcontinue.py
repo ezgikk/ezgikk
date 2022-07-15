45feab61958be68ea8a3b5daf3e0f2127c0a15a2
ezgikk <80405937+ezgikk@users.noreply.github.com>
2022-07-15 13:53:42 +0300

    Create breakcontinue.py

---
+++ breakcontinue.py
@@ -0,0 +1,16 @@
+shopping=["milk","egg","pasta","spam","bread","rice"]
+#for item in shopping:
+#    if item != "spam":
+#        print("buy " + item)
+
+for item in shopping:
+    if item =="spam":
+        continue
+    print("buy " + item)
+
+    print("buy "+item)
+for item in shopping:
+    if item =="spam":
+        break
+
+    print("buy "+item)
