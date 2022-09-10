9f66b0c66032cd9dd1e7afeaaeed287a0cb5b9cd
ezgikk <80405937+ezgikk@users.noreply.github.com>
2022-09-10 21:10:40 +0300

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,15 @@
+for t in enumerate("abcdefgh"):
+    print(t)
+
+for a in enumerate("highschool"):
+    index,character= a
+    print(index,character)
+metallica="ride the lightning", "metallica", 1984
+title,artist,year=metallica
+print(title)
+print(artist)
+print(year)
+table=("coffee table", 200,100,75,34)
+print(table[1]*table[2])
+name,length,width,height, price = table
+print(length * height)
