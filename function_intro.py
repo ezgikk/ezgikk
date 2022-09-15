33f31c392c7de8f32897d2697011c0cd94dfe48e
ezgikk <80405937+ezgikk@users.noreply.github.com>
2022-09-15 15:06:07 +0300

    Create function_intro.py

---
+++ function_intro.py
@@ -0,0 +1,11 @@
+def multiply(x,y):
+    result=x+y
+    return result
+answer=multiply(10,5)
+print(answer)
+forty=multiply(20,20)
+print(forty)
+
+for val in range(1,5):
+    two_times=multiply(val,2)
+    print(two_times)
